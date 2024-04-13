89c22ed01a14cad912923d1cee4173301c283055
DenizKim1987 <164604681+DenizKim1987@users.noreply.github.com>
2024-04-14 00:48:33 +0900

    toonflix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:toontlix/screens/home_screen.dart';
+import 'package:toontlix/services/api_service.dart';
 
 void main() {
   runApp(const App());
@@ -12,18 +12,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          backgroundColor: const Color(0xFFE7626C),
-        ),
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-      ),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
